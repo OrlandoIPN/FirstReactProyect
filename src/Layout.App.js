58690af3af6376e492dc0a57ae29940fa71dac2e
Orlando Aguilar <orlandoipn@hotmail.com>
2022-02-12 12:57:41 -0600

    was added component Main

--- src/Layout.App.js
+++ src/Layout.App.js
@@ -6,6 +6,7 @@
 import React from 'react';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
+import { Main } from './components/Main';
 import { QueueList } from './components/QueueList';
 
 
@@ -14,22 +15,7 @@ const Layout = () => {
     return <div>
         <Header />
         <div className='contenido-principal'>
-            <main className='blog'>
-                <div className='blog_izquierda'>
-                    <div className='blog_izquiera-contenido'>
-                        <h2>A Mad Tea-Party</h2>
-                        <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </h3>
-                        <hr></hr>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                        <button className='button'>Go to items</button>
-                    </div>
-                </div>
-                <div className='blog_derecha'>
-                    <img src={'../img/tanjiro.png'}
-                        alt='logo'
-                        className='img_logo' />
-                </div>
-            </main>
+            <Main /> 
             <QueueList />
             <Footer />
 
